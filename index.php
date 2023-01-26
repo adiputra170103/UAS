154677777661e3f2064ef6a3e4615e39ea74c8c6
adiputra170103 <123295672+adiputra170103@users.noreply.github.com>
2023-01-26 10:59:31 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hottel</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Mukta+Mahee:200,300,400|Playfair+Display:400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/aos.css">
+    
+    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+    <header class="site-header">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-4 site-logo" data-aos="fade"><a href="index.html"><em>HOME</em></a></div>
+          <div class="col-8">
+
+
+            <div class="site-menu-toggle js-site-menu-toggle"  data-aos="fade">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <!-- END menu-toggle -->
+
+            <div class="site-navbar js-site-navbar">
+              <nav role="navigation">
+                <div class="container">
+                  <div class="row full-height align-items-center">
+                    <div class="col-md-6">
+                      <ul class="list-unstyled menu">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="../login.php">Login</a></li>
+                        <li><a href="../login.php">Booking</a></li>
+                      </ul>
+                    </div>
+                    <div class="col-md-6 extra-info">
+                      <div class="row">
+                        <div class="col-md-6 mb-5">
+                          <h3>Contact Info</h3>
+                          <p>Jalan sepang utara<br> kebun biru 87</p>
+                          <p>hotteloyo@gmail.com</p>
+                          <p>(+60) 104011393</p>
+                          
+                        </div>
+                        <div class="col-md-6">
+                          <h3>Connect With Us</h3>
+                          <ul class="list-unstyled">
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Instagram</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>      
+    </header>
+    <!-- END head -->
+
+    <section class="site-hero overlay" style="background-image: url(img/OYO.jpg)">
+      <div class="container">
+        <div class="row site-hero-inner justify-content-center align-items-center">
+          <div class="col-md-10 text-center">
+            <h1 class="heading" data-aos="fade-up">Welcome to <em>Hottel</em></h1>
+            <p class="sub-heading mb-5" data-aos="fade-up" data-aos-delay="100">Discover our world-class hottel</p>
+          </div>
+        </div>
+        <!-- <a href="#" class="scroll-down">Scroll Down</a> -->
+      </div>
+    </section>
+    <!-- END section -->
+
+    <section class="section visit-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="heading" data-aos="fade-right">You Can Visit</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 visit mb-3" data-aos="fade-right">
+            <a href="restaurant.html"><img src="img/standar1.jpg" alt="Image placeholder" class="img-fluid"> </a>
+            <h3><a href="restaurant.html">Standar Room</a></h3>
+            <div class="reviews-star float-left">
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star-half"></span>
+              <span class="ion-android-star-outline"></span>
+            </div>
+            
+            <span class="reviews-count float-right">
+              3,239 reviews
+            </span>
+          </div>
+          <div class="col-lg-3 col-md-6 visit mb-3" data-aos="fade-right" data-aos-delay="100">
+            <a href="restaurant.html"><img src="img/twin.jpg" alt="Image placeholder" class="img-fluid"> </a>
+            <h3><a href="restaurant.html">Twin Room</a></h3>
+            <div class="reviews-star float-left">
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star-half"></span>
+              <span class="ion-android-star-outline"></span>
+            </div>
+            <span class="reviews-count float-right">
+              4,921 reviews
+            </span>
+          </div>
+          <div class="col-lg-3 col-md-6 visit mb-3" data-aos="fade-right" data-aos-delay="200">
+            <a href="hotel.html"><img src="img/single.jpg" alt="Image placeholder" class="img-fluid"> </a>
+            <h3><a href="hotel.html">Single Room</a></h3>
+            <div class="reviews-star float-left">
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star"></span>
+              <span class="ion-android-star-outline"></span>
+            </div>
+            <span class="reviews-count float-right">
+              2,112 reviews
+            </span>
+          </div>
+          
+          </div>
+          <p data-aos="fade-up" data-aos-delay="100"><a href="../login.php" class="btn uppercase btn-primary mr-md-2 mr-0 mb-3 d-sm-inline d-block">Booking Now</a></p>
+        </div>
+      </div>
+    </section>
+
+    <footer class="section footer-section">
+      <div class="text-center">
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> </a>
+      </div>
+     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            
+      </div>
+    </footer>
+    
+    
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- <script src="js/jquery.waypoints.min.js"></script> -->
+    <script src="js/aos.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
